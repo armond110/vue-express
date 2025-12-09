f2ab27cb2d15a739981a25ec498efa24966046dd
Armond Kurbalaj <armondkurbalaj1@gmail.com>
2025-12-09 18:08:31 +0100

    first commit

--- server/index.js
+++ server/index.js
@@ -6,12 +6,21 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 //Middleware
-app.use(cors());
 app.use(bodyParser.json());
+app.use(cors());
 
 const posts = require('./routes/api/posts');
 
 app.use('/api/posts', posts);
+
+// Handle production
+if (process.env.NODE_ENV === 'production') {
+  // Static folder
+  app.use(express.static(__dirname + '/public/'));
+
+  // Handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
